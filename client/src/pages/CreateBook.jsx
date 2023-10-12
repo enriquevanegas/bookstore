8fa400971302c15f11dfe9cef65663e63e8e84fb
EVAN <enriquevanegas@outlook.com>
2023-10-12 00:53:15 -0600

    App routes
    App routes have been created

---
+++ client/src/pages/CreateBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateBook = () => {
+    return (
+        <div>
+            Create Book
+        </div>
+    )
+}
+
+export { CreateBook }
---
+++ client/src/pages/ShowBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowBook = () => {
+    return (
+        <div>
+            Show Books
+        </div>
+    )
+}
+
+export { ShowBook }
